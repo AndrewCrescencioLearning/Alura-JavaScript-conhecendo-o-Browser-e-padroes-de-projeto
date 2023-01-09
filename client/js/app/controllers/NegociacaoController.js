c079dac5a4246cdaca4c567066c0d4b17a42d440
Andrew Crescencio dos Santos <68707868+AndrewCrescencio@users.noreply.github.com>
2023-01-09 15:19:31 -0300

    :sparkles: feat: NegociacaoController

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,22 @@
+class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document);
+    this._inputQuantidade = $("#quantidade");
+    this._inputData = $("#data");
+    this._inputValor = $("#valor");
+  }
+
+  adiciona(event) {
+    event.preventDefault();
+
+    let negociacao = new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade,
+      this._inputValor
+    );
+
+    console.log(negociacao);
+
+    console.log(DateHelper.dataParaTexto(negociacao.data));
+  }
+}
